243baf88dfd3086f6629daad4923f62880600671
Mia <Lia@Lias-MacBook-Pro.local>
2016-05-13 09:29:06 +0900

    initial commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/schap843/pen/MypOXO.
+
+ Pomodoro Clock for FreeCodeCamp. Length of study time and break time can be adjusted. Keeps track of session number and plays a bell sound at the end of each session and break,
---
+++ js/index.js
@@ -0,0 +1,125 @@
+$(document).ready(function() {
+  $(".settings").hide();
+  $('#pause').hide();
+  $(".fa").click(function(){
+    $(".settings").toggle();
+  });
+});
+
+var myInterval;
+var workTime = document.getElementById("work").innerHTML=25;
+var restTime = document.getElementById("break").innerHTML=5;
+var remainSec = workTime*60;
+var sessionNumber = 1;
+document.getElementById("timer").innerHTML=workTime + ":00";
+function startWork(){
+  document.getElementById("start").disabled = true;
+  timer(callback);
+  $(".settings").hide();
+  $('#start').hide();
+  $('#pause').show();
+  $('#status').show();
+  $('#statusBottom').show();
+};
+function pauseTime(){
+  clearInterval(myInterval);
+  document.getElementById("start").disabled = false;
+  $('#start').show();
+  $('#pause').hide();
+}
+function displayTime(remainingTime){
+if(remainingTime%60 >= 10) {
+  document.getElementById('timer').innerHTML=Math.floor(remainingTime/60) + ':' + Math.floor(remainingTime%60);
+   }
+else{
+  document.getElementById('timer').innerHTML=Math.floor(remainingTime/60) + ':' + "0" + Math.floor(remainingTime%60) ;
+    }
+}
+function timer(cb) {
+  var remainingTime = remainSec;
+  document.getElementById('status').innerHTML="Session " + sessionNumber;
+  document.getElementById('statusBottom').innerHTML="Session " + sessionNumber;
+  myInterval=setTimeout(function() {  
+    displayTime(remainingTime);
+    if (remainingTime >= 0) {
+      remainSec--;
+      timer(cb); 
+    }
+    else{
+    clearInterval();
+    cb();
+    }
+  }, 1000);
+}
+
+var callback = function() {
+  console.log('callback');
+  $('#status').hide();
+  $('#breakTime').text("Break Time!");
+  $('#statusBottom').hide();
+  $('#breakTimeBottom').text("Break Time!");
+  document.getElementById('timer').innerHTML="Break Time";
+  remainSec=restTime*60;
+  timer(callbackRest)
+  document.getElementById('bell').play();
+
+};
+var callbackRest = function() {
+  clearInterval(myInterval);
+  console.log('callbackRest');
+  sessionNumber++;
+  $('#status').show();
+  $('#breakTime').hide();
+  $('#statusBottom').show();
+  $('#breakTimeBottom').hide();
+  document.getElementById('status').innerHTML="Session " + sessionNumber;
+  document.getElementById('statusBottom').innerHTML="Session " + sessionNumber;
+  document.getElementById('timer').innerHTML="Session " + sessionNumber;
+  document.getElementById('bell').play();
+  remainSec=workTime*60;
+  document.getElementById("start").disabled = true;
+  $('#start').hide();
+  $('#pause').show();
+  timer(callback);
+};
+function resetTime(){
+  clearInterval(myInterval);
+  remainSec=workTime*60;
+  document.getElementById("start").disabled = false;
+  sessionNumber = 1;
+  $('#start').show();
+  $('#pause').hide();
+  $('#status').hide();
+  $('#statusBottom').hide();
+  $('#breakTime').hide();
+  $('#breakTimeBottom').hide();
+  document.getElementById("timer").innerHTML=workTime + ":00"
+}
+function minusWork(){
+  if(workTime >= 2) {
+  pauseTime();
+  document.getElementById('work').innerHTML=--workTime;
+  document.getElementById("timer").innerHTML=workTime + ":00";
+  remainSec=workTime*60;
+  }
+}
+function minusBreak(){
+  if(restTime>=2) {pauseTime();document.getElementById('break').innerHTML=--restTime;}
+}
+function plusWork(){
+  pauseTime();
+  document.getElementById('work').innerHTML=++workTime;
+  document.getElementById("timer").innerHTML=workTime + ":00";
+  remainSec=workTime*60;
+}
+function plusBreak(){
+  pauseTime();
+  document.getElementById('break').innerHTML=++restTime;
+}
+document.getElementById('start').addEventListener('click', startWork);
+document.getElementById('pause').addEventListener('click', pauseTime);
+document.getElementById('reset').addEventListener('click', resetTime);
+document.getElementById('minw').addEventListener('click',minusWork);
+document.getElementById('minb').addEventListener('click',minusBreak);
+document.getElementById('pluw').addEventListener('click',plusWork);
+document.getElementById('plub').addEventListener('click',plusBreak);
